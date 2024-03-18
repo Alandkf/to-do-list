79ae8137b2a339dfbbb060089b8e4966a195beb6
AlandKf <aland.zland@gmail.com>
2024-03-18 17:30:51 +0300

    I kinda finished the to-do-list now it can insert and delete data works just fine

--- app.js
+++ app.js
@@ -1,68 +1,79 @@
 const express = require('express');
-// const bodyParser = require('body-parser'); no longer needed since Express 4.16.0+
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Start now
-
 const uri = "mongodb://localhost:27017/todolistDB";
 
-mongoose.connect(uri).then(()=>{console.log("connected to database successfully")
-}).catch(err=>console.log("ERROR WE DATABASE DID NOT CONNECTED" + err));
+mongoose.connect(uri).then(() => {
+    console.log("Connected to database successfully");
+}).catch(err => console.log("Error connecting to the database: " + err));
 
-
-const dbSchema = new mongoose.Schema ({
-    item:{type : String , required :true}, 
+const dbSchema = new mongoose.Schema({
+    item: { type: String, required: true },
 });
 const Item = mongoose.model("Item", dbSchema);
 
-const Item1 = new Item({item:"Welcome to your to-do-list"})
-const Item2 = new Item({item:"Hit the + button to add a new item"});
-const Item3 = new Item({item:"<-- and hit this to delete a new item"});
+const Item1 = new Item({ item: "Welcome to your to-do-list" })
+const Item2 = new Item({ item: "Hit the + button to add a new item" });
+const Item3 = new Item({ item: "<-- and hit this to delete a new item" });
 const defaultItem = [Item1, Item2, Item3];
 
-Item.insertMany(defaultItem).then(()=>{console.log("default item inserted");})
-.catch(err=>{console.log("error in inserting default items"+err)});
-
-// Item1.save().then(()=>{console.log("new Item saved successfully");})
-// .catch(()=>{console.log("failed to save the new Item");})
-
-// Item.deleteOne({_id:"65f825425322acbc51d6312e"})
-// .then(()=>{console.log("deleted")})
-// .catch(err=>{console.log("error in deleting an item "+err)})
-
-
-
-
-
-
-
-
-
 app.get('/', (req, res) => {
-
-    res.render('list',{DAY:"today" , newItems:newItems});
+    Item.find({})
+        .then((items) => {
+            if (items.length === 0) {
+                return Item.insertMany(defaultItem)
+                    .then(() => {
+                        console.log("Default items inserted");
+                        res.render('list', { DAY: "today", newItems: defaultItem });
+                    })
+                    .catch((err) => {
+                        console.log("Error inserting default items: " + err);
+                        res.status(500).send("Internal Server Error");
+                    });
+            } else {
+                res.render('list', { DAY: "today", newItems: items });
+            }
+        })
+        .catch((err) => {
+            console.log("Error finding items: " + err);
+            res.status(500).send("Internal Server Error");
+        });
 });
+
+   
+
+    // .catch(err=>{
+    //     console.log("error in finding items "+err)
+    //     mongoose.connection.close()
+    // });
+    
 
 app.post("/",(req, res) =>{
-    let newItem = req.body.newItem;
+    const itemName = req.body.newItem;
+    let newItem = new Item({ item: itemName })
+    newItem.save().then(() =>{ 
+        res.redirect("/")
+    })
+})
 
-    if(req.body.list === "work"){
-        workItems.push(newItem);
-        res.redirect('/work')
-    } else{
-        newItems.push(newItem);
-        res.redirect("/")  
-    }
-});
+
+app.post('/delete',(req, res) =>{
+    const id = req.body.checkbox;
+    Item.deleteOne({_id: id}).then(()=>{
+        res.redirect("/")
+    })
+})
+
 
 app.get('/work',(req,res)=>{
     res.render('list',{DAY:"work list", newItems:workItems})
